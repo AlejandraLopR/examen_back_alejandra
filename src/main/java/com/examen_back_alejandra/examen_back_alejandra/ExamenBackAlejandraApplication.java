bdffe1d6fae560d530beb819d034f6ba69e96f46
Alejandra Lopez Rojas <alejandra.lopezzrojas@gmail.com>
2025-06-12 14:38:58 -0600

    Primer coomit - Examen_back

---
+++ src/main/java/com/examen_back_alejandra/examen_back_alejandra/ExamenBackAlejandraApplication.java
@@ -0,0 +1,13 @@
+package com.examen_back_alejandra.examen_back_alejandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenBackAlejandraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenBackAlejandraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examen_back_alejandra/examen_back_alejandra/ExamenBackAlejandraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examen_back_alejandra.examen_back_alejandra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenBackAlejandraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
